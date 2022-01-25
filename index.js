a567f71abff86b1e09d1c2f87abb6ba683a1f30e
Michael Severino <mikeyseverino@gmail.com>
2022-01-24 20:29:49 -0500

    commit tree 46c38b16f49d4125513fb2b0f5c91915c9982f37

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-// Write your solution in this file!
+var customerName = 'bob';
+var bestCustomer = 'maybe bob';
+const leastFavoriteCustomer = 'Allison'
+
+function upperCaseCustomerName(){
+    return customerName.toUpperCase();
+}
+
+function setBestCustomer(){
+    var bestCustomer = 'not bob';
+    return bestCustomer;
+}
+
+function overwriteBestCustomer(){
+    return bestCustomer;
+}
+
+function changeLeastFavoriteCustomer(){
+    var leastFavoriteCustomer = "Kim"
+    return leastFavoriteCustomer;
+}
+
